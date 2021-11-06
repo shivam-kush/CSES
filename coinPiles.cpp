4dd870a4700a3406a3991f2bb08634dc833a6ac2
Shivam Kushwaha <shivamkush.2000@gmail.com>
2021-11-07 01:57:53 +0530

    dk

---
+++ coinPiles.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	long long int t,a,b;
+	cin >> t;
+	while(t--){
+		cin >> a >> b;
+		if((a+b)%3==0 && (min(a,b))*2 >= max(a,b)) cout << "YES" << endl;
+		else cout << "NO" << endl;
+		return 0;
+}
+}
